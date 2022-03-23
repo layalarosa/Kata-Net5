4108ad7a555d400db9af8efcb866631fc9bff79d
layalarosa <layalarosa@gmail.com>
2022-03-23 15:31:36 -0400

    Added model and core folders, the superhero entity and the controller were created.

---
+++ Kata-Net5/Models/SuperHero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kata_Net5.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
